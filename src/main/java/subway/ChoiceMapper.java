ef27da70b53174000e253d6a2ffdc70b5d140e45
dldbdud314 <dldbdud314@naver.com>
2022-12-02 21:06:23 +0900

    chore(ChoiceMapper): 안 쓰는 필드 삭제

--- src/main/java/subway/ChoiceMapper.java
+++ src/main/java/subway/ChoiceMapper.java
@@ -1,17 +1,8 @@
 package subway;
 
-import java.util.Arrays;
 import subway.controller.Controller;
-import subway.controller.LineController;
-import subway.controller.SectionController;
-import subway.controller.StationController;
-import subway.controller.SubwayPrinter;
 
 public class ChoiceMapper {
-    private LineController lineController;
-    private SectionController sectionController;
-    private StationController stationController;
-    private SubwayPrinter subwayPrinter;
 
     public Controller executeByUserChoice(String choice){
         return MainControls
