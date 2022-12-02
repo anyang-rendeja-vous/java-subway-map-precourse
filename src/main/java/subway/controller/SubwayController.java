ba0c88539d0338349312f2648d59872bf14b91ea
minju412 <ln8847@ajou.ac.kr>
2022-12-02 13:28:30 +0900

    feat: create initial lines
    - 주어진 사전 등록 정보로 노선을 등록한다

--- src/main/java/subway/controller/SubwayController.java
+++ src/main/java/subway/controller/SubwayController.java
@@ -2,6 +2,8 @@ package subway.controller;
 
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.domain.Station;
 import subway.domain.StationRepository;
 import subway.view.InputView;
@@ -21,9 +23,19 @@ public class SubwayController {
         initialSettings();
         outputView.printMenu();
         int choice = Integer.parseInt(inputView.inputNumber());
+
+
+//        System.out.println(StationRepository.stations().stream()
+//                .map(Station::getName)
+//                .collect(Collectors.toList()));
     }
 
     private void initialSettings() {
+        setStations();
+        setLines();
+    }
+
+    private void setStations() {
         createStation("교대역");
         createStation("강남역");
         createStation("역삼역");
@@ -35,5 +47,15 @@ public class SubwayController {
 
     private void createStation(String stationName) {
         StationRepository.addStation(new Station(stationName));
+    }
+
+    private void setLines() {
+        createLine("2호선");
+        createLine("3호선");
+        createLine("신분당선");
+    }
+
+    private void createLine(String lineName) {
+        LineRepository.addLine(new Line(lineName));
     }
 }
