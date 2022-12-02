5fcb7eb4d61fca69341c123f573db42b4cd7c0ee
dldbdud314 <dldbdud314@naver.com>
2022-12-02 15:04:45 +0900

    feat(StationController, InputReader): 역 관리 클래스 메소드 템플릿

---
+++ src/main/java/subway/InputReader.java
@@ -0,0 +1,11 @@
+package subway;
+
+import java.util.Scanner;
+
+public class InputReader {
+    final Scanner scanner = new Scanner(System.in);
+
+    public String getUserInput(){
+        return scanner.next();
+    }
+}
--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -1,12 +1,49 @@
 package subway.controller;
 
+import subway.InputReader;
+import subway.domain.StationRepository;
+
 public class StationController implements Controller {
 
+    private final InputReader inputReader;
+
     public StationController() {
+        inputReader = new InputReader();
     }
 
     @Override
     public void execute() {
-
+        String choice = inputReader.getUserInput();
+        executeChoice(choice);
     }
+
+    // to fix..
+    private void executeChoice(String choice){
+        if (choice.equals("1")){
+            createStation();
+        }
+        if (choice.equals("2")){
+            deleteStation();
+        }
+        if (choice.equals("3")){
+            getStationsList();
+        }
+        if (choice.equals("B")){
+            return;
+        }
+        throw new IllegalArgumentException("error!");
+    }
+
+    private void createStation(){
+        StationRepository.addStation();
+    }
+
+    private void deleteStation(){
+        StationRepository.deleteStation();
+    }
+
+    private void getStationsList(){
+        StationRepository.stations();
+    }
+
 }
