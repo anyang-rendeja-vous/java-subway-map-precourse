bc2fb0e96165bc69a60d3d358fbc36aac3ed29fe
dldbdud314 <dldbdud314@naver.com>
2022-12-02 21:04:24 +0900

    fix(LineController): 개선된 메소드 매핑 적용

--- src/main/java/subway/controller/LineController.java
+++ src/main/java/subway/controller/LineController.java
@@ -1,48 +1,53 @@
 package subway.controller;
 
+import static subway.ui.ErrorMessages.INVALID_MAIN_CHOICE;
+
+import java.util.HashMap;
+import java.util.Map;
 import subway.InputReader;
-import subway.domain.LineRepository;
 import subway.ui.InputView;
 import subway.ui.OutputView;
 
 public class LineController implements Controller {
+    private static final String CREATE = "1";
+    private static final String DELETE = "2";
+    private static final String GET_LIST = "3";
+    private static final String BACK = "B";
 
     private final InputReader inputReader;
     private final InputView inputView;
     private final OutputView outputView;
+    private final Map<String, Runnable> commandMap = new HashMap<>();
 
 
     public LineController() {
         inputReader = new InputReader();
         inputView = new InputView();
         outputView = new OutputView();
+        mapInit();
+    }
+
+    private void mapInit() {
+        commandMap.put(CREATE, this::createLine);
+        commandMap.put(DELETE, this::deleteLine);
+        commandMap.put(GET_LIST, this::getLinesList);
+        commandMap.put(BACK, this::goBack);
     }
 
     @Override
     public void execute() {
         inputView.printLineChoicesMenu();
-        String choice = inputReader.getUserInput();
-        executeChoice(choice);
+        runCommand();
     }
 
-    // to fix..
-    private void executeChoice(String choice){
-        if (choice.equals("1")){
-            createLine();
-            return;
+    private void runCommand() {
+        try {
+            String choice = inputReader.getUserInput();
+            commandMap.getOrDefault(choice, this::throwException).run();
+        } catch (IllegalArgumentException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            runCommand();
         }
-        if (choice.equals("2")){
-            deleteLine();
-            return;
-        }
-        if (choice.equals("3")){
-            getLinesList();
-            return;
-        }
-        if (choice.equals("B")){
-            return;
-        }
-        throw new IllegalArgumentException("error!");
     }
 
     private void createLine() {
@@ -52,6 +57,14 @@ public class LineController implements Controller {
     }
 
     private void getLinesList() {
+    }
+
+    private void goBack() {
+        return;
+    }
+
+    private void throwException() {
+        throw new IllegalArgumentException(INVALID_MAIN_CHOICE.getMessage());
     }
 
 }
