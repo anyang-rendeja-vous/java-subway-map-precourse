b9fb88ab3e05c952bf91f7cc042d96f9be8e1ce6
dldbdud314 <dldbdud314@naver.com>
2022-12-02 13:49:23 +0900

    feat(ChoiceMapper, MainControls): 컨트롤러 매핑 로직 추가

--- src/main/java/subway/ChoiceMapper.java
+++ src/main/java/subway/ChoiceMapper.java
@@ -1,2 +1,22 @@
-package subway;public class ChoiceMapper {
+package subway;
+
+import java.util.Arrays;
+import subway.controller.LineController;
+import subway.controller.SectionController;
+import subway.controller.StationController;
+import subway.controller.SubwayPrinter;
+
+public class ChoiceMapper {
+    private LineController lineController;
+    private SectionController sectionController;
+    private StationController stationController;
+    private SubwayPrinter subwayPrinter;
+
+    public MainControls executeByUserChoice(String choice){
+        return Arrays.stream(MainControls.values())
+                .filter(x -> x.isMatchingController(choice))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 선택할 수 없는 기능입니다."));
+    }
+
 }
--- src/main/java/subway/MainControls.java
+++ src/main/java/subway/MainControls.java
@@ -1,2 +1,31 @@
-package subway;public enum MainControls {
+package subway;
+
+import java.util.function.Supplier;
+import subway.controller.Controller;
+import subway.controller.LineController;
+import subway.controller.SectionController;
+import subway.controller.StationController;
+import subway.controller.SubwayPrinter;
+
+public enum MainControls {
+    STATION_CONTROL("1", StationController::new),
+    LINE_CONTROL("2", LineController::new),
+    SECTION_CONTROL("3", SectionController::new),
+    LINES_PRINTER("4", SubwayPrinter::new);
+
+    private String choice;
+    private Supplier<Controller> controllerMaker;
+
+    MainControls(String choice, Supplier<Controller> controllerMaker) {
+        this.choice = choice;
+        this.controllerMaker = controllerMaker;
+    }
+
+    public Controller generatedController(){
+        return controllerMaker.get();
+    }
+
+    public boolean isMatchingController(String choice){
+        return choice.equals(this.choice);
+    }
 }
