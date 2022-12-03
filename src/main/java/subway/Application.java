fd0cf522dcb6bb36ea4c09a84f40552d653ad654
dldbdud314 <dldbdud314@naver.com>
2022-12-03 15:46:41 +0900

    feat(Application): 잘못된 입력 에러 처리 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -5,18 +5,29 @@ import static subway.InputReader.getUserInput;
 
 import subway.controller.Controller;
 import subway.ui.InputView;
+import subway.ui.OutputView;
 
 public class Application {
     public static boolean isContinue = true;
 
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        while (isContinue){
-            inputView.printMainChoicesMenu();
-            inputView.printUsersChoiceOpening();
-            Controller controller = executeByUserChoice(getUserInput());
+        OutputView outputView = new OutputView();
+
+        while (isContinue) {
+            Controller controller = getMatchingController(inputView, outputView);
             controller.execute();
         }
+    }
 
+    private static Controller getMatchingController(InputView inputView, OutputView outputView) {
+        inputView.printMainChoicesMenu();
+        inputView.printUsersChoiceOpening();
+        try {
+            return executeByUserChoice(getUserInput());
+        } catch (IllegalArgumentException exception) {
+            outputView.printErrorMessage(exception.getMessage());
+            return getMatchingController(inputView, outputView);
+        }
     }
 }
