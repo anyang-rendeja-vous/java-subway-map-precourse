6315f78c15a28259e71a5336bc573c1a969c3e7e
dldbdud314 <dldbdud314@naver.com>
2022-12-06 12:31:37 +0900

    feat(ProgramFinish): 프로그램 종료 안내 멘트

--- src/main/java/subway/controller/ProgramFinish.java
+++ src/main/java/subway/controller/ProgramFinish.java
@@ -1,10 +1,14 @@
 package subway.controller;
 
 import subway.Application;
+import subway.ui.OutputView;
 
 public class ProgramFinish implements Controller{
+    private final OutputView outputView = new OutputView();
+
     @Override
     public void execute() {
         Application.isContinue = false;
+        outputView.printApplicationEnd();
     }
 }
