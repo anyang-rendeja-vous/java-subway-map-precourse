e7b9c5d35aaabd3e1504557b5de4b72d05ff3649
minju412 <ln8847@ajou.ac.kr>
2022-12-06 19:31:15 +0900

    refactor: add menu
    - 쉬운 유지보수를 위해 메뉴 추가

---
+++ src/main/java/subway/domain/menu/LineManageMenu.java
@@ -0,0 +1,33 @@
+package subway.domain.menu;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public enum LineManageMenu {
+
+    MAIN("##", " 노선 관리 화면"),
+    ADD_LINE("1", ". 노선 등록"),
+    DELETE_LINE("2", ". 노선 삭제"),
+    SELECT_LINE("3", ". 노선 조회"),
+    BACK("B", ". 돌아가기");
+
+    private final String prefix;
+    private final String content;
+
+    LineManageMenu(String prefix, String content) {
+        this.prefix = prefix;
+        this.content = content;
+    }
+
+    public static List<String> getAll() {
+        return Arrays.stream(LineManageMenu.values())
+                .map(LineManageMenu::toString)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return prefix + content;
+    }
+}
--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -4,30 +4,12 @@ import java.util.List;
 import subway.domain.Line;
 import subway.domain.LineRepository;
 import subway.domain.Station;
+import subway.domain.menu.LineManageMenu;
+import subway.domain.menu.MainMenu;
+import subway.domain.menu.SectionManageMenu;
+import subway.domain.menu.StationManageMenu;
 
 public class OutputView {
-
-    private static final String MAIN = "## 메인 화면";
-    private static final String STATION_MANAGEMENT = "1. 역 관리";
-    private static final String LINE_MANAGEMENT = "2. 노선 관리";
-    private static final String SECTION_MANAGEMENT = "3. 구간 관리";
-    private static final String PRINT_SUBWAY_MAP = "4. 지하철 노선도 출력";
-    private static final String QUIT = "Q. 종료";
-
-    private static final String STATION_MANAGEMENT_MAIN = "## 역 관리 화면";
-    private static final String ADD_STATION = "1. 역 등록";
-    private static final String DELETE_STATION = "2. 역 삭제";
-    private static final String PRINT_ALL_STATIONS = "3. 역 조회";
-    private static final String BACK = "B. 돌아가기";
-
-    private static final String LINE_MANAGEMENT_MAIN = "## 노선 관리 화면";
-    private static final String ADD_LINE = "1. 노선 등록";
-    private static final String DELETE_LINE = "2. 노선 삭제";
-    private static final String PRINT_ALL_LINES = "3. 노선 조회";
-
-    private static final String SECTION_MANAGEMENT_MAIN = "## 구간 관리 화면";
-    private static final String ADD_SECTION = "1. 구간 등록";
-    private static final String DELETE_SECTION = "2. 구간 삭제";
 
     private static final String PREFIX = "[INFO] ";
     private static final String SUCCESSFUL_STATION_REGISTRATION = "지하철 역이 등록되었습니다.";
@@ -49,32 +31,29 @@ public class OutputView {
     }
 
     public void printInitialMenu() {
-        System.out.println(MAIN);
-        System.out.println(STATION_MANAGEMENT);
-        System.out.println(LINE_MANAGEMENT);
-        System.out.println(SECTION_MANAGEMENT);
-        System.out.println(PRINT_SUBWAY_MAP);
-        System.out.println(QUIT);
+        MainMenu.getAll()
+                .forEach(System.out::println);
         System.out.println();
     }
 
     public void printStationManagementMenu() {
         System.out.println();
-        System.out.println(STATION_MANAGEMENT_MAIN);
-        System.out.println(ADD_STATION);
-        System.out.println(DELETE_STATION);
-        System.out.println(PRINT_ALL_STATIONS);
-        System.out.println(BACK);
+        StationManageMenu.getAll()
+                        .forEach(System.out::println);
         System.out.println();
     }
 
     public void printLineManagementMenu() {
         System.out.println();
-        System.out.println(LINE_MANAGEMENT_MAIN);
-        System.out.println(ADD_LINE);
-        System.out.println(DELETE_LINE);
-        System.out.println(PRINT_ALL_LINES);
-        System.out.println(BACK);
+        LineManageMenu.getAll()
+                .forEach(System.out::println);
+        System.out.println();
+    }
+
+    public void printSectionManagementMenu() {
+        System.out.println();
+        SectionManageMenu.getAll()
+                .forEach(System.out::println);
         System.out.println();
     }
 
@@ -113,15 +92,6 @@ public class OutputView {
     public void successDeleteLine() {
         System.out.println();
         System.out.println(PREFIX + SUCCESSFUL_LINE_DELETE);
-        System.out.println();
-    }
-
-    public void printSectionManagementMenu() {
-        System.out.println();
-        System.out.println(SECTION_MANAGEMENT_MAIN);
-        System.out.println(ADD_SECTION);
-        System.out.println(DELETE_SECTION);
-        System.out.println(BACK);
         System.out.println();
     }
 
