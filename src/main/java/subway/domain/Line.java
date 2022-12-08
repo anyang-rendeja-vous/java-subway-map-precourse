855b6e86326479489f823ab2cb4d0b6e115c6f1c
minju412 <ln8847@ajou.ac.kr>
2022-12-08 10:16:33 +0900

    fix(Line): add validation
    - 구간을 추가할 때 (= 노선에 역을 추가할 때)
      올바르지 않은 순서를 입력할 경우 예외 처리
      - ex. 현재 노선에 역이 총 3개인데 5번째에 추가하고자 하는 경우

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -7,6 +7,7 @@ public class Line {
     private static final String LINE_ALREADY_EXISTED_ERROR = "이미 등록된 노선 이름입니다.";
     private static final String LINE_INVALID_SIZE_ERROR = "지하철 노선 이름은 2글자 이상이어야 합니다.";
     private static final String CANNOT_DELETE_STATION_ERROR = "노선에 포함된 역이 두 개 이하인 경우 역을 제거할 수 없습니다.";
+    private static final String INVALID_ORDER_ERROR = "순서는 1 부터 %d 까지 가능합니다.";
     private String name;
     private LinkedList<Station> stations = new LinkedList<>();
 
@@ -43,8 +44,10 @@ public class Line {
         stations.add(1, station);
     }
 
-    // 지정한 위치에 추가
     public void addSectionAsSpecified(Station station, int order) {
+        if (order < 1 || order > stations.size()) {
+            throw new IllegalArgumentException(String.format(INVALID_ORDER_ERROR, stations.size() + 1));
+        }
         stations.add(order, station);
     }
 
